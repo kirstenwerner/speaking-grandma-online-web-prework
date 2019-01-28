0d7bf6fe6a138020be66a47256d4b082ae357e3c
Kirsten Werner <kirstenmwerner@gmail.com>
2019-01-28 22:45:43 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,6 +4,6 @@ def speak_to_grandma(string)
   elsif string == string.upcase
     return "NO, NOT SINCE 1938!"
   else 
-    return "HUH?! SPEAK UP, sonny!"
+    return "HUH?! SPEAK UP, SONNY!"
   end
 end
